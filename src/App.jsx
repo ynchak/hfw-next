10cab5627de8475d9d5e76105c759a221ea0ec23
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2023-11-26 15:48:50 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Divider } from "@nextui-org/react";
+import { Card, CardBody, Divider } from "@nextui-org/react";
 import FormAction from "./components/FormAction";
 import {
   makeLinks,
@@ -18,9 +18,13 @@ const App = () => {
       <div className="container mx-auto">
         <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
           <div className="mt-2">
-            <h1 className="text-center text-2xl font-medium">
-              Перенесення товарів
-            </h1>
+            <Card>
+              <CardBody>
+                <h1 className="text-center text-xl font-medium">
+                  Перенесення товарів
+                </h1>
+              </CardBody>
+            </Card>
             <Divider className="mt-2" />
             <FormAction {...openSourcesWithGoodsConfig} />
             <FormAction {...transformConfig} />
@@ -28,9 +32,13 @@ const App = () => {
             <FormAction {...splitColumn} />
           </div>
           <div className="mt-2">
-            <h1 className="text-center text-2xl font-medium">
-              Оновлення товарів
-            </h1>
+            <Card>
+              <CardBody>
+                <h1 className="text-center text-xl font-medium">
+                  Оновлення товарів
+                </h1>
+              </CardBody>
+            </Card>
             <Divider className="mt-2" />
             <FormAction {...openSourcesAttr} />
             <FormSave />
