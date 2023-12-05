7c4f62c532fc711b2f11485e6d50b8ad0f0e6e5b
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2023-12-05 16:02:22 +0200

    complete action for form is categoty clean

--- src/formConfigs/checkIsCategoryClean.js
+++ src/formConfigs/checkIsCategoryClean.js
@@ -1,27 +1,40 @@
-import useSaveFormStore from '../store';
-import { openInNewTab } from '../utils';
+import useSaveFormStore from "../store";
+import { openInNewTab } from "../utils";
 
-const maxLines = 50;
+const addres =
+  "https://gomer.rozetka.company/gomer/smart-folders/next-page?searchParams[1][field]=sync_source_id&searchParams[1][condition]=%3D&searchParams[1][value]=";
+const params =
+  "&searchParams[2][field]=category&searchParams[2][condition]=%3D&searchParams[2][value]=";
+const attr = "&pageCondition=all&page=0&pageSize=50&size=50";
+const maxLines = 30;
 const action = (column) => {
   const { variant, value } = useSaveFormStore.getState();
-  if (variant !== 'category' || !variant) {
+  if (variant !== "category" || !variant) {
     throw `❌ Форма вишче ☝️ не налаштована`;
   }
-  const lines = column.split('\n');
+  const lines = column.split("\n");
   if (lines.length > maxLines + 1) {
     // last line is empty
     throw `Введено більше ${maxLines} джерел`;
   }
-  const link = ``;
-  console.log(lines.join('+'));
-  // openInNewTab([link]);
+  const links = [];
+  for (const line of lines) {
+    if (!line) {
+      continue;
+    }
+
+    links.push();
+  }
+  const link = `${addres}${lines.join(",")}${params}${value}${attr}`;
+  // console.log(link);
+  openInNewTab([link]);
 };
 export default {
-  buttonColor: 'primary',
-  buttonIcon: 'open',
-  buttonTitle: 'До конструктора умов',
+  buttonColor: "primary",
+  buttonIcon: "open",
+  buttonTitle: "До конструктора умов",
   placeholder:
-    'Перевірити чи залишились товари в категорії.\nВстав стовпчик з id джерелами',
+    "Перевірити чи залишились товари в категорії.\nВстав стовпчик з id джерелами",
   radioGroup: null,
   action,
 };
